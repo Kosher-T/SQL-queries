dae68063087c1868a74b3b6fe3596a92454cb16e
Itoro Usa <169055253+Kosher-T@users.noreply.github.com>
2025-06-25 08:08:03 +0100

    Update Imaginary products.sql
    Added tables for the exercise

--- Imaginary products.sql
+++ Imaginary products.sql
@@ -1,3 +1,97 @@
+-- New exercise. Hardest one yet
+-- Table: enom_gilpane
+CREATE TABLE enom_gilpane (
+    part_id TEXT,
+    rpm REAL,
+    watt INTEGER,
+    size INTEGER
+);
+
+-- Populate enom_gilpane
+INSERT INTO enom_gilpane (part_id, rpm, watt, size) VALUES
+('A', 4269.372, 95, 7),
+('B', 8914.598, 174, 4),
+('C', 7853.7, 36, 6),
+('D', 3984.036, 146, 16),
+('A', 8728.528, 90, 17),
+('B', 1711.437, 192, 12),
+('C', 6773.778, 46, 1),
+('D', 9083.355, 198, 13),
+('A', 2556.883, 190, 19),
+('B', 3191.102, 161, 11),
+('C', 6398.457, 126, 18),
+('D', 8614.086, 38, 20);
+
+-- Table: castle_loctus
+CREATE TABLE castle_loctus (
+    part_id TEXT,
+    rpm REAL,
+    watt INTEGER,
+    size INTEGER
+);
+
+-- Populate castle_loctus
+INSERT INTO castle_loctus (part_id, rpm, watt, size) VALUES
+('A', 2973.576, 150, 1),
+('C', 6886.279, 42, 7),
+('D', 2329.252, 191, 16),
+('E', 4690.612, 74, 15),
+('A', 3415.696, 125, 3),
+('C', 5503.339, 138, 8),
+('D', 2047.412, 93, 19),
+('E', 1887.138, 71, 17),
+('A', 3075.848, 39, 2),
+('C', 4399.997, 133, 4),
+('D', 3979.093, 154, 18),
+('E', 4811.204, 31, 13);
+
+-- Table: honpan_bilopsa
+CREATE TABLE honpan_bilopsa (
+    part_id TEXT,
+    rpm REAL,
+    watt INTEGER,
+    size INTEGER
+);
+
+-- Populate honpan_bilopsa
+INSERT INTO honpan_bilopsa (part_id, rpm, watt, size) VALUES
+('B', 7095.499, 85, 22),
+('D', 5020.034, 58, 11),
+('E', 8782.956, 87, 9),
+('F', 5885.662, 62, 7),
+('B', 7151.229, 81, 15),
+('D', 5111.166, 45, 6),
+('E', 5429.338, 54, 5),
+('F', 4936.521, 90, 16),
+('B', 6588.933, 95, 21),
+('D', 5355.704, 27, 19),
+('E', 5625.185, 91, 25),
+('F', 8738.879, 76, 8);
+
+-- Table: yurnol_qoltam
+CREATE TABLE yurnol_qoltam (
+    part_id TEXT,
+    rpm REAL,
+    watt INTEGER,
+    size INTEGER
+);
+
+-- Populate yurnol_qoltam
+INSERT INTO yurnol_qoltam (part_id, rpm, watt, size) VALUES
+('F', 4126.755, 74, 20),
+('C', 3602.962, 102, 9),
+('A', 4341.742, 112, 7),
+('B', 3322.368, 144, 25),
+('F', 4313.828, 88, 27),
+('C', 3157.77, 143, 21),
+('A', 4510.035, 121, 22),
+('B', 4897.276, 54, 29),
+('F', 4755.144, 61, 30),
+('C', 3147.466, 81, 10),
+('A', 3806.276, 150, 17),
+('B', 4089.562, 70, 14);
+
+
 WITH
   -- Step 1: Calculate the quality for each part in the 'enom_gilpane' table.
   -- The quality formula is (rpm * (watt + avg_watt_for_table)) / size.
